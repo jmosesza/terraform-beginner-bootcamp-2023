533c3ff753e181b01b3a74fb5f3ab4e8e602dac5
Justin Moses <justin.z.moses@gmail.com>
2023-10-09 10:09:41 +0000

    #29 create cdn and serve s3 bucket from cdn

---
+++ modules/terrahome_aws/resource-storage.tf
@@ -0,0 +1,84 @@
+#provider "aws" {
+# # Configuration options
+#}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "website_bucket" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  # we want to assign a random bucket name
+  #bucket = var.bucket_name
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
+}
+
+# Create an S3 bucket for the static website
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
+
+#resource "aws_s3_bucket_website_configuration" "website_configuration" {
+#  bucket = aws_s3_bucket.website_bucket.bucket # Change to a unique name for your bucket
+#
+# index_document {
+#    suffix = "index.html"
+#  }
+
+#  error_document {
+#    key = "error.html"
+#  }
+#}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  source = var.index_html_filepath
+  #source = "${var.public_path}/index.html"
+  content_type = "text/html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  #etag = filemd5("${var.public_path}/index.html")
+  etag = filemd5(var.index_html_filepath)
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"
+  #source = "${var.public_path}/error.html"
+  source = var.error_html_filepath
+  content_type = "text/html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  #etag = filemd5("${var.public_path}/error.html")
+  etag = filemd5(var.error_html_filepath)
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  #policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+  policy = jsonencode({
+    "Version" = "2012-10-17",
+    "Statement" = {
+      "Sid" = "AllowCloudFrontServicePrincipalReadOnly",
+      "Effect" = "Allow",
+      "Principal" = {
+        "Service" = "cloudfront.amazonaws.com"
+      },
+      "Action" = "s3:GetObject",
+      "Resource" = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
+      "Condition" = {
+      "StringEquals" = {
+          #"AWS:SourceArn": data.aws_caller_identity.current.arn
+          "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+        }
+      }
+    }
+  })
+}

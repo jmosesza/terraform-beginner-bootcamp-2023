ee0e933ff770f484b7bed1ac66ea2795f166e232
jmosesza <62349969+jmosesza@users.noreply.github.com>
2023-10-09 23:38:01 +0200

    Update outputs.tf

--- modules/terrahome_aws/outputs.tf
+++ modules/terrahome_aws/outputs.tf
@@ -2,6 +2,20 @@ output "bucket_name" {
   value = aws_s3_bucket.website_bucket.bucket
 }
 
+output "website_endpoint" {
+  description = "website endpoint for the statically hosted website"
+  value       = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+  description = "The CloudFront distribution domain name"
+ }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
+
 #output "website_endpoint" {
 #  value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
 #}

356df6695a3c5aa163319b775f0addf5b60ae539
Justin Moses <justin.z.moses@gmail.com>
2023-10-09 02:31:48 +0000

    #27 bled static website hosting, uploaded index and error file via terraform

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "Bucket name for our static website hosting"
   value = module.terrahome_aws.bucket_name  
 }
+
+#output "s3_website_endpoint" {
+#  description = "S3 Static Website hosting endpoint"
+#  value = module.terrahome_aws.website_endpoint
+#}

b9366c984c55b1c51911d2006a5cd8d851aa4de9
jmosesza <62349969+jmosesza@users.noreply.github.com>
2023-10-11 00:43:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ module "terrahome_aws" {
   source = "./modules/terrahome_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
+  index_html_filepath = "${path.root}${var.index_html_filepath}"
+  error_html_filepath = "${path.root}${var.error_html_filepath}"
   assets_path = var.assets_path
   content_version = var.content_version
 }

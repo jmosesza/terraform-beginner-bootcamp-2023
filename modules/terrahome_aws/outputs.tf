4bb4d67122a3a486d55fb61c14e541448fd70cf0
jmosesza <62349969+jmosesza@users.noreply.github.com>
2023-10-11 06:58:55 +0200

    Update outputs.tf

--- modules/terrahome_aws/outputs.tf
+++ modules/terrahome_aws/outputs.tf
@@ -13,9 +13,9 @@ output "cloudfront_url" {
  
  }
 
-output "cloudfront_distribution_id" {
-  value = aws_cloudfront_distribution.s3_distribution.id
-}
+#output "cloudfront_distribution_id" {
+#  value = aws_cloudfront_distribution.s3_distribution.id
+#}
 
 #output "website_endpoint" {
 #  value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint

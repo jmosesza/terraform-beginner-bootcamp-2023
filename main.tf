8a5f373967afe825c2f238d41545ec1d1d5cfa90
jmosesza <62349969+jmosesza@users.noreply.github.com>
2023-11-08 22:00:51 +0200

    42 terratowns provider (#44)
    * #42 wip setting up the skeleton for creating a custom terraform provider
    
    * #42 implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+   required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
   #backend "remote" {
   #  hostname = "app.terraform.io"
   #  organization = "jmosesza"
@@ -15,16 +21,25 @@ terraform {
   #}
 }
 
+provider "terratowns" {
+  #endpoint = var.terratowns_endpoint
+  #user_uuid = var.teacherseat_user_uuid
+  #token = var.terratowns_access_token
+  endpoint = "http://localhost:4567"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
 
-module "terrahome_aws" {
-  source = "./modules/terrahome_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
+
+#module "terrahome_aws" {
+#  source = "./modules/terrahome_aws"
+#  user_uuid = var.user_uuid
+#  bucket_name = var.bucket_name
+#  index_html_filepath = var.index_html_filepath
+#  error_html_filepath = var.error_html_filepath
   #index_html_filepath = "${path.root}${var.index_html_filepath}"
   #error_html_filepath = "${path.root}${var.error_html_filepath}"
-  assets_path = var.assets_path
-  content_version = var.content_version
-  root_path = var.root_path
-}
+# assets_path = var.assets_path
+#  content_version = var.content_version
+#  root_path = var.root_path
+#}

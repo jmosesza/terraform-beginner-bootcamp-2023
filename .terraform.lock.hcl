43fcd454e8e0eddb8ce72f84f6d9fd65afb7c873
Justin Moses <justin.z.moses@gmail.com>
2023-11-11 12:55:10 +0000

    #42 finish implementation for terraform provider

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,6 +5,6 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:M4vUbAXPg68WOsMnOTOZhH8mwTvvouDMAXT4w1yIjYY=",
+    "h1:ZmbJHrJywX3vJlVfKPwLjI1nNcrNInDZ+VT/8Zdqbws=",
   ]
 }
--- main.tf
+++ main.tf
@@ -25,7 +25,8 @@ provider "terratowns" {
   #endpoint = var.terratowns_endpoint
   #user_uuid = var.teacherseat_user_uuid
   #token = var.terratowns_access_token
-  endpoint = "http://localhost:4567"
+  #endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -43,3 +44,20 @@ provider "terratowns" {
 #  content_version = var.content_version
 #  root_path = var.root_path
 #}
+
+resource "terratowns_home" "home" {
+  name = "How to play Arcanum in 2023!"
+  description = <<DESCRIPTION
+Arcanum is a game from 2001 that shipped with alot of bugs.
+Modders have removed all the originals making this game really fun
+to play (despite that old look graphics). This is my guide that will
+show you how to play arcanum without spoiling the plot.
+DESCRIPTION
+  #domain_name = module.home_arcanum_hosting.domain_name
+  #town = "missingo"
+  #content_version = var.arcanum.content_version
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}

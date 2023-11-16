5d698c85d453a36ee7ae148316dffc81c1d26b46
Justin Moses <justin.z.moses@gmail.com>
2023-11-16 23:25:23 +0000

    #50 update

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,13 @@
 output "bucket_name" {
   description = "Bucket name for our static website hosting"
-  value = module.terrahome_aws.bucket_name  
+#  value = module.terrahome_aws.bucket_name
+  value = module.home_scblacklist_hosting.bucket_name 
 }
 
 output "s3_website_endpoint" {
   description = "S3 Static Website hosting endpoint"
-  value = module.terrahome_aws.website_endpoint
+#  value = module.terrahome_aws.website_endpoint
+  value = module.home_scblacklist_hosting.website_endpoint
 }
 
 #output "cloudfront_url" {
@@ -15,7 +17,8 @@ output "s3_website_endpoint" {
 
 output "cloudfront_url" {
   description = "The CloudFront Distribution Domain Name"
-  value = module.terrahome_aws.domain_name
+#  value = module.terrahome_aws.domain_name
+  value = module.home_scblacklist_hosting.domain_name
 }
 
 
